383514b3ebe0c4cf85e83a8c5da7b1e6590447b0
MR <a.m.rasouli.n@gmail.com>
2024-03-22 14:34:21 +0330

    Added song data

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ xml_file_path = os.path.expanduser("~/.local/share/rhythmbox/rhythmdb.xml")
 sum_duration = 0
 sum_play_count = 0
 
+most_played_song = None
+most_played_song_play_count = 0
+
+second_most_played_song = None
+second_most_played_song_play_count = 0
+
 tree = ET.parse(xml_file_path)
 root = tree.getroot()
 
@@ -17,7 +23,14 @@ for entry in root.findall('./entry[@type="song"]'):
     play_count = int(play_count_element.text) if play_count_element is not None else 0
     sum_duration += duration
     sum_play_count += play_count
-
+    
+    if most_played_song_play_count < play_count:
+    	second_most_played_song = most_played_song
+    	second_most_played_song_play_count = most_played_song_play_count
+    
+    	most_played_song = entry
+    	most_played_song_play_count = play_count
+    	
 background_images_dir = 'images'
 
 background_images = [file for file in os.listdir(background_images_dir) if file.endswith('.jpg')]
@@ -50,16 +63,32 @@ image.paste(background_image, (0, 0))
 draw = ImageDraw.Draw(image)
 
 title_font = ImageFont.truetype(font_path, size=48)
-summary_font = ImageFont.truetype(font_path, size=36)
+summary_font = ImageFont.truetype(font_path, size=26)
+sub_summary_font = ImageFont.truetype(font_path, size=22)
 panel_font = ImageFont.truetype(font_path, size=10)
 
 draw.text((20, 20), f"Rhythmbox-Wrapped", fill='white', font=title_font)
 
 draw.text((18, 74), f"/"*75, fill='white', font=panel_font)
 
-draw.text((20, 110), f"Duration: {sum_duration}", fill='white', font=summary_font)
+draw.text((20, 100), f"Total Time Listened: {sum_duration}", fill='white', font=summary_font)
 
-draw.text((20, 176), f"Play Count: {sum_play_count}", fill='white', font=summary_font)
+draw.text((20, 136), f"Play Count: {sum_play_count}", fill='white', font=summary_font)
+
+draw.text((20, 172), f"1st Most Played Song:", fill='white', font=summary_font)
+draw.text((40, 202), f"Title: {most_played_song.find('title').text}", fill='white', font=sub_summary_font)
+draw.text((40, 232), f"Artist: {most_played_song.find('artist').text}", fill='white', font=sub_summary_font)
+draw.text((40, 262), f"Album: {most_played_song.find('album').text}", fill='white', font=sub_summary_font)
+draw.text((40, 292), f"Genre: {most_played_song.find('genre').text}", fill='white', font=sub_summary_font)
+draw.text((40, 322), f"Play Count: {most_played_song.find('play-count').text}", fill='white', font=sub_summary_font)
+
+draw.text((20, 358), f"2nd Most Played Song:", fill='white', font=summary_font)
+draw.text((40, 388), f"Title: {second_most_played_song.find('title').text}", fill='white', font=sub_summary_font)
+draw.text((40, 418), f"Artist: {second_most_played_song.find('artist').text}", fill='white', font=sub_summary_font)
+draw.text((40, 448), f"Album: {second_most_played_song.find('album').text}", fill='white', font=sub_summary_font)
+draw.text((40, 478), f"Genre: {second_most_played_song.find('genre').text}", fill='white', font=sub_summary_font)
+draw.text((40, 508), f"Play Count: {second_most_played_song.find('play-count').text}", fill='white', font=sub_summary_font)
+
 
 draw.text((20, 540), f"By using the \"Rhythmbox-wrapped\" you are implying that you genuinely dislike \"Spotify\" as a platform for listening to music.", fill='white', font=panel_font)
 
