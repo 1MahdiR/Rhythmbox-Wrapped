779efcdf7cc4a7904a52fa854d4889e7c5531592
MR <a.m.rasouli.n@gmail.com>
2024-03-22 15:43:04 +0330

    Fixed showing total time, changed fonts

--- main.py
+++ main.py
@@ -31,6 +31,12 @@ for entry in root.findall('./entry[@type="song"]'):
     	most_played_song = entry
     	most_played_song_play_count = play_count
     	
+sum_duration_minutes = sum_duration // 60
+sum_duration_secends = sum_duration % 60
+
+sum_duration_hours = sum_duration_minutes // 60
+sum_duration_minutes = sum_duration_minutes % 60
+
 background_images_dir = 'images'
 
 background_images = [file for file in os.listdir(background_images_dir) if file.endswith('.jpg')]
@@ -55,32 +61,32 @@ image.paste(background_image, (0, 0))
 	
 draw = ImageDraw.Draw(image)
 
-title_font = ImageFont.truetype("fonts/TimmanaRegular.ttf", size=48)
+title_font = ImageFont.truetype("fonts/Campton-BoldDEMO.otf", size=36)
 summary_font = ImageFont.truetype("fonts/Manjari-Regular.otf", size=26)
 sub_summary_font = ImageFont.truetype("fonts/Manjari-Thin.otf", size=22)
-panel_font = ImageFont.truetype("fonts/TimmanaRegular.ttf", size=10)
+panel_font = ImageFont.truetype("fonts/Manjari-Thin.otf", size=10)
 
 draw.text((20, 20), f"Rhythmbox-Wrapped", fill='white', font=title_font)
 
-draw.text((18, 74), f"/"*75, fill='white', font=panel_font)
+draw.text((18, 44), f"_"*30, fill='white', font=summary_font)
 
-draw.text((20, 100), f"Total Time Listened: {sum_duration}", fill='white', font=summary_font)
+draw.text((20, 85), f"Total Time Listened: {sum_duration_hours}:{sum_duration_minutes}':{sum_duration_secends}\"", fill='white', font=summary_font)
 
-draw.text((20, 136), f"Play Count: {sum_play_count}", fill='white', font=summary_font)
+draw.text((20, 123), f"Play Count: {sum_play_count}", fill='white', font=summary_font)
 
-draw.text((20, 172), f"1st Most Played Song:", fill='white', font=summary_font)
-draw.text((40, 202), f"Title: {most_played_song.find('title').text}", fill='white', font=sub_summary_font)
-draw.text((40, 232), f"Artist: {most_played_song.find('artist').text}", fill='white', font=sub_summary_font)
-draw.text((40, 262), f"Album: {most_played_song.find('album').text}", fill='white', font=sub_summary_font)
-draw.text((40, 292), f"Genre: {most_played_song.find('genre').text}", fill='white', font=sub_summary_font)
-draw.text((40, 322), f"Play Count: {most_played_song.find('play-count').text}", fill='white', font=sub_summary_font)
+draw.text((20, 157), f"1st Most Played Song:", fill='white', font=summary_font)
+draw.text((40, 187), f"Title: {most_played_song.find('title').text}", fill='white', font=sub_summary_font)
+draw.text((40, 217), f"Artist: {most_played_song.find('artist').text}", fill='white', font=sub_summary_font)
+draw.text((40, 247), f"Album: {most_played_song.find('album').text}", fill='white', font=sub_summary_font)
+draw.text((40, 277), f"Genre: {most_played_song.find('genre').text}", fill='white', font=sub_summary_font)
+draw.text((40, 307), f"Play Count: {most_played_song.find('play-count').text}", fill='white', font=sub_summary_font)
 
-draw.text((20, 358), f"2nd Most Played Song:", fill='white', font=summary_font)
-draw.text((40, 388), f"Title: {second_most_played_song.find('title').text}", fill='white', font=sub_summary_font)
-draw.text((40, 418), f"Artist: {second_most_played_song.find('artist').text}", fill='white', font=sub_summary_font)
-draw.text((40, 448), f"Album: {second_most_played_song.find('album').text}", fill='white', font=sub_summary_font)
-draw.text((40, 478), f"Genre: {second_most_played_song.find('genre').text}", fill='white', font=sub_summary_font)
-draw.text((40, 508), f"Play Count: {second_most_played_song.find('play-count').text}", fill='white', font=sub_summary_font)
+draw.text((20, 343), f"2nd Most Played Song:", fill='white', font=summary_font)
+draw.text((40, 373), f"Title: {second_most_played_song.find('title').text}", fill='white', font=sub_summary_font)
+draw.text((40, 403), f"Artist: {second_most_played_song.find('artist').text}", fill='white', font=sub_summary_font)
+draw.text((40, 433), f"Album: {second_most_played_song.find('album').text}", fill='white', font=sub_summary_font)
+draw.text((40, 463), f"Genre: {second_most_played_song.find('genre').text}", fill='white', font=sub_summary_font)
+draw.text((40, 493), f"Play Count: {second_most_played_song.find('play-count').text}", fill='white', font=sub_summary_font)
 
 
 draw.text((20, 540), f"By using the \"Rhythmbox-wrapped\" you are implying that you genuinely dislike \"Spotify\" as a platform for listening to music.", fill='white', font=panel_font)
